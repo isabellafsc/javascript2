cd1dd58d8e0a0618f7200be064586ab208580543
isabellafsc <isabellafscarpari@gmail.com>
2022-10-27 16:34:01 -0300

    aula081

--- aula079-087 - Jogo de nave/main.js
+++ aula079-087 - Jogo de nave/main.js
@@ -1,48 +1,13 @@
-//Elementos
-var jogador
-
-//Controle de animação
-var frame
-
-//Posições
-var posJogadorX, posJogadorY
-
-//Direção
-var dirJogadorY, dirJogadorX
-
-//Tamanhos
-var telaWidth, telaHeight
-//Velocidade
-var velJogador
-
-//Controle
-var tecla
+var jogador                     //Elementos
+var frames                      //Controle de animação
+var posJogadorX, posJogadorY    //Posições
+var dirJogadorY, dirJogadorX    //Direção
+var telaWidth, telaHeight       //Tamanhos
+var velJogador                  //Velocidade
+var tecla                       //Controle
 var jogo = false
 
-function inicia() {
-  jogo = false
-  telaWidth = window.innerWidth
-  telaHeight = window.innerHeight
-  //JOGADOR INICIAL:
-  dirJogadorX = dirJogadorY = 0
-  posJogadorX = telaWidth / 2
-  posJogadorY = telaHeight / 2
-  velJogador = 5
-  jogador = document.getElementById('naveJogador')
-  jogador.style.top = posJogadorY + 'px'
-  jogador.style.left = posJogadorX + 'px'
-}
-
-function gameLoop() {
-  if (jogo) {
-    //FUNÇÃO DE CONTROLE
-  }
-  frame = requestAnimationFrame(gameLoop)
-}
-
-function controlaJogador() {}
-
-function teclaDown(event) {
+function teclaDown(event) {  //MOVER NAVE
   tecla = event.key
   if (tecla == 'ArrowUp') {
     dirJogadorY = -1
@@ -53,16 +18,59 @@ function teclaDown(event) {
   } else if (tecla == 'ArrowRight') {
     dirJogadorX = 1
   } else if (tecla == ' ') {
+    atira(posJogadorX + 18, posJogadorY)
   }
 }
 
-function teclaUp(event) {
+function teclaUp(event) {   //PARAR NAVE
   tecla = event.key
   if (tecla == 'ArrowUp' || tecla == 'ArrowDown') {
     dirJogadorY = 0
   } else if (tecla == 'ArrowLeft' || tecla == 'ArrowRight') {
     dirJogadorX = 0
   }
+}
+
+function atira(x, y) {
+  var tiro = document.createElement('div')
+  var atributo1 = document.createAttribute('class')
+  var atributo2 = document.createAttribute('style')
+  atributo1.value = 'tiroJogador'
+  atributo2.value = 'top:' + y + 'px;left:' + x + 'px'
+  tiro.setAttributeNode(atributo1)
+  tiro.setAttributeNode(atributo2)
+  document.body.appendChild(tiro)
+}
+
+function controlaJogador() {
+  posJogadorY += dirJogadorY * velJogador
+  posJogadorX += dirJogadorX * velJogador
+  jogador.style.top = posJogadorY + 'px'
+  jogador.style.left = posJogadorX + 'px'
+}
+
+function gameLoop() {
+  if (jogo) {
+    //FUNÇÃO DE CONTROLE
+    controlaJogador()
+  }
+  frames = requestAnimationFrame(gameLoop)
+}
+
+function inicia() {
+  jogo = true
+  telaWidth = window.innerWidth
+  telaHeight = window.innerHeight
+  //JOGADOR INICIAL:
+  dirJogadorX = dirJogadorY = 0
+  posJogadorX = telaWidth / 2
+  posJogadorY = telaHeight / 2
+  velJogador = 5
+  jogador = document.getElementById('naveJogador')
+  jogador.style.top = posJogadorY + 'px'
+  jogador.style.left = posJogadorX + 'px'
+
+  gameLoop()
 }
 
 document.addEventListener('keydown', teclaDown)
