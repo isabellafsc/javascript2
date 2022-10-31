b21368ff2812aad2d0a15342eb6c2ad616054e19
isabellafsc <isabellafscarpari@gmail.com>
2022-10-31 12:28:41 -0300

    aula083

--- aula079-087 - Jogo de nave/main.js
+++ aula079-087 - Jogo de nave/main.js
@@ -1,22 +1,24 @@
-var jogador, bomba //Elementos
+var jogador //Elementos
 var frames //Controle de animação
 var posJogadorX, posJogadorY //Posições
 var dirJogadorY, dirJogadorX //Direção
 var telaWidth, telaHeight //Tamanhos
-var velJogador, velTiro //Velocidade
+var velJogador, velTiro, velBomba //Velocidade
 var tecla //Controle
 var jogo = false
+var contagemBombas, painelContBombas, intervaloBomba
+var hpPlaneta
 
 function teclaDown(event) {
   //MOVER NAVE
   tecla = event.key
-  if (tecla == 'ArrowUp') {
+  if (tecla == 'ArrowUp' || tecla == 'w') {
     dirJogadorY = -1
-  } else if (tecla == 'ArrowDown') {
+  } else if (tecla == 'ArrowDown' || tecla == 's') {
     dirJogadorY = 1
-  } else if (tecla == 'ArrowLeft') {
+  } else if (tecla == 'ArrowLeft' || tecla == 'a') {
     dirJogadorX = -1
-  } else if (tecla == 'ArrowRight') {
+  } else if (tecla == 'ArrowRight' || tecla == 'd') {
     dirJogadorX = 1
   } else if (tecla == ' ') {
     atira(posJogadorX + 18, posJogadorY)
@@ -24,18 +26,27 @@ function teclaDown(event) {
 }
 
 function teclaUp(event) {
-  //PARAR NAVE
   tecla = event.key
-  if (tecla == 'ArrowUp' || tecla == 'ArrowDown') {
+  if (
+    tecla == 'ArrowUp' ||
+    tecla == 'ArrowDown' ||
+    tecla == 'w' ||
+    tecla == 's'
+  ) {
     dirJogadorY = 0
-  } else if (tecla == 'ArrowLeft' || tecla == 'ArrowRight') {
+  } else if (
+    tecla == 'ArrowLeft' ||
+    tecla == 'ArrowRight' ||
+    tecla == 'a' ||
+    tecla == 'd'
+  ) {
     dirJogadorX = 0
   }
 }
 
 function criaBomba() {
   if (jogo) {
-    bomba = document.createElement('div')
+    var bomba = document.createElement('div')
     var posBombaX = Math.random() * telaWidth
     var posBombaY = 0
     var atributo1 = document.createAttribute('class')
@@ -45,6 +56,22 @@ function criaBomba() {
     bomba.setAttributeNode(atributo1)
     bomba.setAttributeNode(atributo2)
     document.body.appendChild(bomba)
+    contagemBombas--
+  }
+}
+
+function controleBombas() {
+  var bombas = document.getElementsByClassName('bomba')
+  for (var i = 0; i < bombas.length; i++) {
+    if (bombas[i]) {
+      var posBomba = bombas[i].offsetTop
+      posBomba += velBomba
+      bombas[i].style.top = posBomba + 'px'
+      if (posBomba > telaHeight) {
+        hpPlaneta -= 1
+        bombas[i].remove()
+      }
+    }
   }
 }
 
@@ -85,6 +112,7 @@ function gameLoop() {
     //FUNÇÃO DE CONTROLE
     controleJogador()
     controleTiros()
+    controleBombas()
   }
   frames = requestAnimationFrame(gameLoop)
 }
@@ -93,7 +121,7 @@ function inicia() {
   jogo = true
   telaWidth = window.innerWidth
   telaHeight = window.innerHeight
-  //JOGADOR INICIAL:
+
   dirJogadorX = dirJogadorY = 0
   posJogadorX = telaWidth / 2
   posJogadorY = telaHeight / 2
@@ -101,6 +129,13 @@ function inicia() {
   jogador = document.getElementById('naveJogador')
   jogador.style.top = posJogadorY + 'px'
   jogador.style.left = posJogadorX + 'px'
+
+  clearInterval(intervaloBomba)
+  contagemBombas = 150
+  velBomba = 3
+  intervaloBomba = setInterval(criaBomba, 1700)
+
+  hpPlaneta = 100
 
   gameLoop()
 }
